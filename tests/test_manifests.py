37cea7cd49e9f626d1ea6614fdb80e4c68259784
Brett <brettgraham@gmail.com>
2025-04-21 12:09:09 -0400

    add fixtures

--- tests/test_manifests.py
+++ tests/test_manifests.py
@@ -3,16 +3,11 @@ Test that the manifest file is correctly structured and refers
 to schemas that exist.
 """
 
-from pathlib import Path
-
 import asdf
 import pytest
 import yaml
 
-RESOURCES_ROOT = Path(__file__).absolute().parent.parent / "resources"
 
-
-@pytest.mark.parametrize("manifest_path", (RESOURCES_ROOT / "manifests").glob("**/*.yaml"))
 def test_manifest(manifest_path):
     content = manifest_path.read_bytes()
     manifest = yaml.safe_load(content)
